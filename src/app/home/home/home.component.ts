01a0377b6a2cd4ce1767a3e622318f4b0b371fd8
David Francis <dfran1991@gmail.com>
2017-10-01 11:59:43 -0700

    Update home page

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,17 +1,23 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Constants } from '../../common/core/constants';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   public siteTitle = Constants.Instance.siteTitle;
+  public siteCaption = 'AFO\'s exclusive card game';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  public viewCards(): void {
+    this.router.navigate(['cards']);
   }
 
 }
