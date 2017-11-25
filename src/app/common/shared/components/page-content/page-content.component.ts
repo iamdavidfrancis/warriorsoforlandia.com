da7908eb12bcc9970253d79076b2762d99c1e7dc
David Francis <dfran1991@gmail.com>
2017-11-25 15:06:13 -0800

    Add last updated and update links

--- src/app/common/shared/components/page-content/page-content.component.ts
+++ src/app/common/shared/components/page-content/page-content.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PageContentComponent implements OnInit {
   @Input() cssClass: string;
+  @Input() lastUpdated: string;
 
   constructor() { }
 
