18e6f99d9dbc141b73422e221a4639929c5e2ad5
David Francis <iamdavidfrancis@gmail.com>
2022-07-12 17:23:50 -0700

    Fix file name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/warriors-app'),
+      dir: require('path').join(__dirname, './coverage/warriors-site'),
       subdir: '.',
       reporters: [
         { type: 'html' },
